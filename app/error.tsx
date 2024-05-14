8511a0de154b4a7032c7716f8e725c9294bd107e
Pedro-Estevao <contato@pedroestevao.com>
2024-05-14 10:46:34 -0300

    Fix error localStorage

--- app/error.tsx
+++ app/error.tsx
@@ -1,30 +1,29 @@
-'use client' 
- 
+'use client'
+
+import { useAppContext } from "@/contexts/appContext"
+import { resetContext } from "@/lib/utilFunctions"
 import { useEffect } from 'react'
- 
+
 export default function Error({
-  error,
-  reset,
+	error,
+	reset,
 }: {
-  error: Error
-  reset: () => void
+	error: Error
+	reset: () => void
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+	const { setIntroduction, setStudyPlatform } = useAppContext();
+
+	useEffect(() => {
+		resetContext(setIntroduction, setStudyPlatform);
+		console.error(error)
+	}, [error, setIntroduction, setStudyPlatform])
+
+	return (
+		<div>
+			<h2>Something went wrong!</h2>
+			<button onClick={() => reset()}>
+				Try again
+			</button>
+		</div>
+	)
 }
